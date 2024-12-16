ff79d47327f6bf97f31768c4c469a224ae04b348
unknown <sumerilal1997.lko@gmail.com>
2024-12-16 15:02:37 +0530

    Files added to update

--- app_ota.py
+++ app_ota.py
@@ -3,8 +3,8 @@ from misc import Power
 
 def run_app_ota():
     fota = app_fota.new()
-    download_list = [{'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/master/main.py', 'file_name': '/usr/main.py'}, {'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/master/modbus_RTU.py', 'file_name': '/usr/modbus_RTU.py'},
-                     {'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/master/app_ota.py', 'file_name': '/usr/app_ota.py'}, {'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/master/ota_handler.py', 'file_name': '/usr/ota_handler.py'}]
+    download_list = [{'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/main/main.py', 'file_name': '/usr/main.py'}, {'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/main/modbus_RTU.py', 'file_name': '/usr/modbus_RTU.py'},
+                     {'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/main/app_ota.py', 'file_name': '/usr/app_ota.py'}, {'url': 'https://raw.githubusercontent.com/SumeriLal/quectel_ota/main/ota_handler.py', 'file_name': '/usr/ota_handler.py'}]
     print("Downloading application file.")
     fota.bulk_download(download_list)
     fota.set_update_flag()
